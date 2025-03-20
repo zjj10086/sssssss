22790d26b1369a7f0bb71278e5b475d913931d26
张倔倔 <120997850+zjj10086@users.noreply.github.com>
2025-03-20 11:40:27 +0800

    Update ny.sh

--- ny.sh
+++ ny.sh
@@ -5,8 +5,6 @@ echo -e "nyanpass\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanp
 echo -e "1\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t 6b360d90-2c13-4e8d-b7d9-1ae9ff3a3da8 -u https://materelay.com"
 echo -e "2\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t dfaa7102-e050-4622-9505-15b3f5cb0eee -u https://ny.pgupy.com"
 echo -e "3\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t f5aad45f-ee22-4503-a37e-4e9120f407d8 -u https://ny.trx1.cyou"
-echo -e "bl\ny\ny" | bash <(curl -fLSs https://dl.nyafw.com/download/nyanpass-install.sh) rel_nodeclient "-o -t d2d67c61-1f59-4a83-a9c9-82a15745ba07 -u https://api.izf.la"
-
 # 执行新的 bash 命令（无预输入）
 
 # 安装 Nezha 监控代理
