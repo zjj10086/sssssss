27444f5b7ec0d3bbb1fb1e16f04c9ff119d29985
张倔倔 <120997850+zjj10086@users.noreply.github.com>
2025-10-05 20:40:01 +0800

    Update and rename 4.sh to 5.sh

--- 4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo -e "tutu119216@gmail.com\n6HuEmDY_BRwvHhbMSsESOie2IOKM61Qvahw0pB8f\n\ny\naws1485-28-12wa.484845845.xyz\n8371117300:AAHHMYPBTx8SAxhfexcw3jTcUqQMVVSOuDc\n6133126873" | bash <(wget -qO- https://raw.githubusercontent.com/mocchen/cssmeihua/mochen/shell/ddns.sh)
---
+++ 5.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bash <(wget -qO- https://raw.githubusercontent.com/mocchen/cssmeihua/mochen/shell/ddns.sh) <<EOF
+tutu119216@gmail.com
+6HuEmDY_BRwvHhbMSsESOie2IOKM61Qvahw0pB8f
+
+
+y
+aws1485-28-12wa.484845845.xyz
+8371117300:AAHHMYPBTx8SAxhfexcw3jTcUqQMVVSOuDc
+6133126873
+EOF
